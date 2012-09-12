2845500f5862091c2cab06f52939bc0b373d2fba
Magne Tørresen <magne.toerresen@atmel.com>
2012-09-12 13:02:35 +0200

    * Tralalala. Hoho!

--- nantest.cxx
+++ nantest.cxx
@@ -1,10 +1,17 @@
-//#include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <math.h>
 
 using namespace std;
 
+/** @brief Doxylainen
+ * 
+ * 
+ * @param argc Antall argument.
+ * @param argv Peikere til argument.
+ * 
+ * @return Alltid alt i orden
+ */
 int main(int argc, char** argv)
 {
     double tallofant = NAN;
